1727858322129130350eeb697fe890d9f0f1db06
mitarcon <mitarcon@gmail.com>
2017-04-16 10:58:51 -0500

    pipes basicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  name:string = "Robert";
+  array:number[] = [1,2,3,4,5,6,7,8,9,0];
 }
